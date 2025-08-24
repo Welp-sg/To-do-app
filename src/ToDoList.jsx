9b40ca83cc845f564183afc79dcc0f86caf1d6ab
Manish Lama <lamamanish771@gmail.com>
2025-08-24 16:27:24 +0545

    Completed

--- src/ToDoList.jsx
+++ src/ToDoList.jsx
@@ -11,19 +11,31 @@ function ToDoList() {
   }
   
   function addTask(){
-
+    if(newTask.trim() !== ''){
+        setTasks([...tasks, newTask]);
+        setNewTask('');
+    }
   }
 
   function deleteTask(index){
-    
+    const updatedTasks = tasks.filter((_, i) => i !== index);
+    setTasks(updatedTasks);
   }
 
   function moveTaskUp(index){
-    
+    if(index > 0){
+      const updatedTasks = [...tasks];
+      [updatedTasks[index - 1], updatedTasks[index]] = [updatedTasks[index], updatedTasks[index - 1]];
+      setTasks(updatedTasks);
+    }
   }
 
   function moveTaskDown(index){
-    
+    if(index < tasks.length - 1){
+      const updatedTasks = [...tasks];
+      [updatedTasks[index + 1], updatedTasks[index]] = [updatedTasks[index], updatedTasks[index + 1]];
+      setTasks(updatedTasks);
+    }
   }
 
   return (
@@ -35,7 +47,15 @@ function ToDoList() {
                 Add Task
             </button>
         </div>
-        <ol> {tasks.map((task,index) => <li key = {index}>{task}</li>)}</ol>
+        <ol> {tasks.map((task,index) =>
+             <li key = {index}>
+                <span className="task">{task}</span>
+                <button className="delete-button" onClick={() => deleteTask(index)}>Delete</button>
+                <button className="move-up-button" onClick={() => moveTaskUp(index)}>Move Up</button>
+                <button className="move-down-button" onClick={() => moveTaskDown(index)}>Move Down</button>
+             </li>
+             )}
+             </ol>
     </div>
   );
 }
